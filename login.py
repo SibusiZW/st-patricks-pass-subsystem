4c2799eaca50bc3c37da588fbe9ed8d5922b6b3c
Tafadzwa Sibanda <sibusitf@gmail.com>
2025-08-12 13:43:47 +0200

    Login form now fully functional ny implementing the verify_user_credentials, login and register_user methods

--- login.py
+++ login.py
@@ -2,6 +2,7 @@
 # Importing of necessary libraries
 import customtkinter as ctk
 import pymysql
+from tkinter import messagebox
 
 class App(ctk.CTk):
     def __init__(self):
@@ -47,8 +48,50 @@ class App(ctk.CTk):
         self.register_button = ctk.CTkButton(self.buttons_frame, corner_radius=5, text="Register", fg_color='green', hover_color='red', command=self.register_user)
         self.register_button.grid(row=0, column=1, padx=10, pady=20)
 
+    def connection(self):
+        # Function which stores connecto
+        try:
+            conn = pymysql.connect(host='localhost', user='root', password='', database='school', autocommit=True)
+            return conn
+        except pymysql.MySQLError as err:
+            messagebox.showerror('Error', err)
+
+    def verify_user_credentials(self, username: str, password: str):
+        # Function that verifies user information in the database
+        try:
+            conn = self.connection()
+            cx = conn.cursor()
+
+            cx.execute("SELECT * FROM passsubsys WHERE user=%s AND password=%s;", (username, password))
+
+            results = cx.fetchone()
+
+            return results
+        except pymysql.MySQLError as err:
+            messagebox.showerror('Error' ,err)
+            
     def login(self):
-        pass
+        if self.verify_user_credentials(self.user_entry.get(), self.passwd_entry.get()):
+            messagebox.showinfo('Success', "Login succesfull!!")
+        else:
+            messagebox.showerror('Error', "User not found!")
+
+        self.user_entry.delete(0, ctk.END)
+        self.passwd_entry.delete(0, ctk.END)
 
     def register_user(self):
-        pass
+        try:
+            # Variables to store entry values
+            username = self.user_entry.get()
+            password = self.passwd_entry.get()
+
+            conn = self.connection()
+            cx = conn.cursor()
+
+            # command to insert values into database table
+            cx.execute("INSERT INTO passsubsys (user, password) VALUES (%s, %s);", (username, password))
+            messagebox.showinfo('Success', "Registered succesfully!")
+        except pymysql.MySQLError as err:
+            messagebox.showerror("Error", f"{err}: Maybe you entered a taken username or the Database server isn't running")
+        self.user_entry.delete(0, ctk.END)
+        self.passwd_entry.delete(0, ctk.END)
