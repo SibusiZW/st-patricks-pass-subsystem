ba846cbee420650e73d03cae8d6b7bf79c4f4cad
Tafadzwa Sibanda <sibusitf@gmail.com>
2025-08-13 11:55:14 +0200

    just changed text for view_recent_button

--- dashboard.py
+++ dashboard.py
@@ -26,7 +26,7 @@ class App(ctk.CTk):
         self.verify_pass_button = ctk.CTkButton(self.buttons_frame, text='Confirm Pass', corner_radius=10, hover_color='green', font=ctk.CTkFont('Cascadia Code', 14, 'bold'), command=self.verify_passes)
         self.verify_pass_button.grid(row=1, column=0, padx=20, pady=20)
 
-        self.view_recent_button = ctk.CTkButton(self.buttons_frame, text='View Recently Confirmed Passes', corner_radius=10, hover_color='green', font=ctk.CTkFont('Cascadia Code', 14, 'bold'))
+        self.view_recent_button = ctk.CTkButton(self.buttons_frame, text='View History Of Confirmed Passes', corner_radius=10, hover_color='green', font=ctk.CTkFont('Cascadia Code', 14, 'bold'))
         self.view_recent_button.grid(row=2, column=0, padx=20, pady=20)
 
     def view_passes(self):
